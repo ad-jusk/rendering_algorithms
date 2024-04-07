208231e993ad889b9a0f76adb2dc6669b9325bd6
Adam <adamj1215@gmail.com>
2024-04-07 22:12:53 +0200

    matrix transforms

--- main.cpp
+++ main.cpp
@@ -3,19 +3,29 @@
 #include "include/Triangle.hpp"
 #include "include/Matrix4.hpp"
 
+#define M_1_180 (1. / 180.)
+
+static float ToRad(float deg) { return static_cast<float>(deg * M_PI * M_1_180); }
+
 int main() {
 
     TgaBuffer buffer(BUFFER_WIDTH, BUFFER_HEIGHT, BG_COLOR);
     Rasterizer rasterizer(&buffer);
 
-    Triangle t(Vector3{-1.f, 0.3f, 0.f}, Vector3{0.f, -1.f, 0.f}, Vector3{1.f, 0.3f, 0.f});
+    Triangle t(Vector3{-1.f, 0.3f, 1.f}, Vector3{0.f, -1.f, 1.f}, Vector3{1.f, 0.3f, 1.f});
 
     Matrix4 transform = Matrix4(1.f);
-    transform *= Matrix4::Perspective(90, 1.f, 0.01f, 100.f);
-    transform *= Matrix4::LookAt(Vector3{0.f, 0.f, 2.f}, Vector3{0, 0, 0}, Vector3(0, 1, 0));
+
+    transform *= Matrix4::Perspective(60, 1.f, 0.01f, 100.f);
+    transform *= Matrix4::Scale(1, 1, 1);
+    transform *= Matrix4::LookAt(Vector3{0.f, 0.f, 6.f}, Vector3{0.f, 0.f, 0.f}, Vector3{0.f, 1.f, 0.f});
+    transform *= Matrix4::Translation(0, 1, 1);
+    transform *= Matrix4::RotationX(ToRad(45));
+    //transform *= Matrix4::RotationY(ToRad(45));
+    transform *= Matrix4::RotationZ(ToRad(90));
 
     t.applyTransform(transform);
-
+    
     rasterizer.drawTriangle(t);
 
     buffer.saveToFile(OUTPUT_PATH_TGA);
